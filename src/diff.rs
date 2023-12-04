f9a334b963c96278998ec4a356c463ffbff0e487
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-12-04 17:34:42 +0000

    Handle file output

--- src/diff.rs
+++ src/diff.rs
@@ -3,13 +3,10 @@ use pyo3::prelude::*;
 use pyo3::types::PyDict;
 use std::io::Write;
 
-#[pyclass]
-struct PyOut(Box<dyn Write + Send>);
-
 pub fn show_diff_trees(
     tree1: &dyn Tree,
     tree2: &dyn Tree,
-    o: Box<dyn Write + Send>,
+    mut w: impl Write,
     old_label: Option<&str>,
     new_label: Option<&str>,
 ) -> PyResult<()> {
@@ -17,7 +14,7 @@ pub fn show_diff_trees(
         let m = py.import("breezy.diff")?;
         let f = m.getattr("show_diff_trees")?;
 
-        let o = PyOut(o);
+        let o = py.import("io")?.call_method0("BytesIO")?;
 
         let kwargs = PyDict::new(py);
         if let Some(old_label) = old_label {
@@ -29,6 +26,10 @@ pub fn show_diff_trees(
         }
 
         f.call((tree1.to_object(py), tree2.to_object(py), o), Some(kwargs))?;
+
+        let s = o.call_method0("getvalue")?.extract::<Vec<u8>>()?;
+
+        w.write_all(&s)?;
 
         Ok(())
     })?;
