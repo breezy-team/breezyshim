7933242bb431b918cfc540df1b3322431dfd17ad
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-02-19 01:20:38 +0000

    Add config

---
+++ src/config.rs
@@ -0,0 +1,34 @@
+pub fn parse_username(e: &str) -> (String, String) {
+    if let Some((_, username, email)) =
+        lazy_regex::regex_captures!(r"(.*?)\s*<?([\[\]\w+.-]+@[\w+.-]+)>?", e)
+    {
+        (username.to_string(), email.to_string())
+    } else {
+        (e.to_string(), "".to_string())
+    }
+}
+
+pub fn extract_email_address(e: &str) -> String {
+    let (_name, email) = parse_username(e);
+    email
+}
+
+#[test]
+fn test_parse_username() {
+    assert_eq!(
+        parse_username("John Doe <joe@example.com>"),
+        ("John Doe".to_string(), "joe@example.com".to_string())
+    );
+    assert_eq!(
+        parse_username("John Doe"),
+        ("John Doe".to_string(), "".to_string())
+    );
+}
+
+#[test]
+fn test_extract_email_address() {
+    assert_eq!(
+        extract_email_address("John Doe <joe@example.com>"),
+        "joe@example.com".to_string()
+    );
+}
--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@
 
 pub mod bazaar;
 pub mod branch;
+pub mod config;
 pub mod controldir;
 pub mod delta;
 pub mod diff;
