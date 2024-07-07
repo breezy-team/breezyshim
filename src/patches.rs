650ba0377c25dee199a2e91e0e4228b29cb4c82e
Jelmer Vernooij <jelmer@jelmer.uk>
2024-07-07 23:24:20 +0000

    Fix AppliedPatches

--- src/patches.rs
+++ src/patches.rs
@@ -14,8 +14,12 @@ fn py_patches(iter_patches: impl Iterator<Item = UnifiedPatch>) -> PyResult<PyOb
         let contextlinec = m.getattr("ContextLine")?;
         let mut ret = vec![];
         for patch in iter_patches {
-            let pypatch =
-                patchc.call1((patch.orig_name, patch.mod_name, patch.orig_ts, patch.mod_ts))?;
+            let pypatch = patchc.call1((
+                PyBytes::new_bound(py, &patch.orig_name),
+                PyBytes::new_bound(py, &patch.mod_name),
+                patch.orig_ts,
+                patch.mod_ts,
+            ))?;
             let pyhunks = pypatch.getattr("hunks")?;
 
             for hunk in patch.hunks {
@@ -73,7 +77,7 @@ pub fn apply_patches(
     })
 }
 
-pub struct AppliedPatches(PyObject);
+pub struct AppliedPatches(PyObject, PyObject);
 
 impl AppliedPatches {
     pub fn new(
@@ -81,23 +85,22 @@ impl AppliedPatches {
         patches: Vec<UnifiedPatch>,
         prefix: Option<usize>,
     ) -> crate::Result<Self> {
-        Python::with_gil(|py| -> Result<_, PyErr> {
+        let (ap, tree) = Python::with_gil(|py| -> Result<_, PyErr> {
             let patches = py_patches(patches.into_iter())?;
             let m = py.import_bound("breezy.patches")?;
             let c = m.getattr("AppliedPatches")?;
-            let tree = c.call1((tree.to_object(py), patches, prefix))?;
-            tree.call_method0("__enter__")?;
-            Ok(tree.to_object(py))
-        })
-        .map_err(Into::into)
-        .map(Self)
+            let ap = c.call1((tree.to_object(py), patches, prefix))?;
+            let tree = ap.call_method0("__enter__")?;
+            Ok((ap.to_object(py), tree.to_object(py)))
+        })?;
+        Ok(Self(tree, ap))
     }
 }
 
 impl Drop for AppliedPatches {
     fn drop(&mut self) {
         Python::with_gil(|py| -> Result<(), PyErr> {
-            self.0
+            self.1
                 .call_method1(py, "__exit__", (py.None(), py.None(), py.None()))?;
             Ok(())
         })
@@ -112,3 +115,88 @@ impl ToPyObject for AppliedPatches {
 }
 
 impl Tree for AppliedPatches {}
+
+#[cfg(test)]
+mod applied_patches_tests {
+    use crate::controldir::ControlDirFormat;
+    use crate::tree::Tree;
+    use patchkit::patch::UnifiedPatch;
+
+    #[test]
+    fn test_apply_simple() {
+        let td = tempfile::tempdir().unwrap();
+        let tree = crate::controldir::create_standalone_workingtree(
+            td.path(),
+            &ControlDirFormat::default(),
+        )
+        .unwrap();
+        std::fs::write(td.path().join("a"), "a\n").unwrap();
+        tree.add(&[std::path::Path::new("a")]).unwrap();
+        tree.commit("Add a", None, None, None).unwrap();
+        let patch = UnifiedPatch::parse_patch(patchkit::parse::splitlines(
+            br#"--- a/a
++++ b/a
+@@ -1 +1 @@
+-a
++b
+"#,
+        ))
+        .unwrap();
+
+        let newtree = crate::patches::AppliedPatches::new(&tree, vec![patch], None).unwrap();
+        assert_eq!(
+            b"b\n".to_vec(),
+            newtree.get_file_text(std::path::Path::new("a")).unwrap()
+        );
+        std::mem::drop(newtree);
+    }
+
+    #[test]
+    fn test_apply_delete() {
+        let td = tempfile::tempdir().unwrap();
+        let tree = crate::controldir::create_standalone_workingtree(
+            td.path(),
+            &ControlDirFormat::default(),
+        )
+        .unwrap();
+        std::fs::write(td.path().join("a"), "a\n").unwrap();
+        tree.add(&[std::path::Path::new("a")]).unwrap();
+        tree.commit("Add a", None, None, None).unwrap();
+        let patch = patchkit::patch::UnifiedPatch::parse_patch(patchkit::parse::splitlines(
+            br#"--- a/a
++++ /dev/null
+@@ -1 +0,0 @@
+-a
+"#,
+        ))
+        .unwrap();
+        let newtree = crate::patches::AppliedPatches::new(&tree, vec![patch], None).unwrap();
+        assert!(!newtree.has_filename(std::path::Path::new("a")));
+    }
+
+    #[test]
+    fn test_apply_add() {
+        let td = tempfile::tempdir().unwrap();
+        let tree = crate::controldir::create_standalone_workingtree(
+            td.path(),
+            &ControlDirFormat::default(),
+        )
+        .unwrap();
+        std::fs::write(td.path().join("a"), "a\n").unwrap();
+        tree.add(&[std::path::Path::new("a")]).unwrap();
+        tree.commit("Add a", None, None, None).unwrap();
+        let patch = patchkit::patch::UnifiedPatch::parse_patch(patchkit::parse::splitlines(
+            br#"--- /dev/null
++++ b/b
+@@ -0,0 +1 @@
++b
+"#,
+        ))
+        .unwrap();
+        let newtree = crate::patches::AppliedPatches::new(&tree, vec![patch], None).unwrap();
+        assert_eq!(
+            b"b\n".to_vec(),
+            newtree.get_file_text(std::path::Path::new("b")).unwrap()
+        );
+    }
+}
