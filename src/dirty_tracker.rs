73bd0847c2dd110f7a67ea684d228064f1d1c150
Jelmer Vernooij <jelmer@jelmer.uk>
2024-07-13 23:00:58 +0000

    Support passing subpath to DirtyTreeTracker

--- src/dirty_tracker.rs
+++ src/dirty_tracker.rs
@@ -5,6 +5,7 @@ pub use dirty_tracker::State;
 pub struct DirtyTreeTracker {
     tracker: DirtyTracker,
     tree: WorkingTree,
+    base: std::path::PathBuf,
 }
 
 impl DirtyTreeTracker {
@@ -12,7 +13,21 @@ impl DirtyTreeTracker {
     pub fn new(tree: WorkingTree) -> Self {
         let base = tree.basedir();
         let tracker = DirtyTracker::new(&base).unwrap();
-        Self { tracker, tree }
+        Self {
+            tracker,
+            tree,
+            base,
+        }
+    }
+
+    pub fn new_in_subpath(tree: WorkingTree, subpath: &std::path::Path) -> Self {
+        let base = tree.basedir();
+        let tracker = DirtyTracker::new(&base.join(subpath)).unwrap();
+        Self {
+            tracker,
+            tree,
+            base,
+        }
     }
 
     /// Get the current state.
@@ -32,8 +47,9 @@ impl DirtyTreeTracker {
 
     /// Get the relative paths of the dirty files.
     pub fn relpaths(&mut self) -> Option<std::collections::HashSet<std::path::PathBuf>> {
-        self.tracker.relpaths().map(|ps| {
-            ps.into_iter()
+        self.tracker.paths().map(|ps| {
+            ps.iter()
+                .map(|p| p.strip_prefix(&self.base).unwrap())
                 .filter(|p| !self.tree.is_control_filename(p))
                 .map(|p| p.to_path_buf())
                 .collect()
@@ -101,8 +117,65 @@ mod tests {
             None,
         )
         .unwrap();
-        assert_eq!(tracker.state(), State::Clean);
         assert_eq!(tracker.relpaths(), Some(std::collections::HashSet::new()));
+        assert_eq!(tracker.state(), State::Clean);
+        assert_eq!(tracker.paths(), Some(std::collections::HashSet::new()));
+    }
+
+    #[test]
+    fn test_in_subpath() {
+        let td = tempfile::tempdir().unwrap();
+
+        let tree = create_standalone_workingtree(td.path(), &ControlDirFormat::default()).unwrap();
+        let subdir = td.path().join("subdir");
+        std::fs::create_dir(&subdir).unwrap();
+        let mut tracker =
+            DirtyTreeTracker::new_in_subpath(tree.clone(), std::path::Path::new("subdir"));
+        std::fs::write(subdir.join("foo"), "bar").unwrap();
+        assert_eq!(
+            tracker.relpaths(),
+            Some(maplit::hashset! { std::path::PathBuf::from("subdir/foo") })
+        );
+        assert_eq!(
+            tracker.paths(),
+            Some(maplit::hashset! { subdir.join("foo") })
+        );
+        assert_eq!(tracker.state(), State::Dirty);
+    }
+
+    #[test]
+    fn test_outside_subpath() {
+        let td = tempfile::tempdir().unwrap();
+
+        let tree = create_standalone_workingtree(td.path(), &ControlDirFormat::default()).unwrap();
+        let subdir = td.path().join("subdir");
+        std::fs::create_dir(subdir).unwrap();
+        let mut tracker =
+            DirtyTreeTracker::new_in_subpath(tree.clone(), std::path::Path::new("subdir"));
+        std::fs::write(td.path().join("foo"), "bar").unwrap();
+        assert_eq!(tracker.relpaths(), Some(std::collections::HashSet::new()));
+        assert_eq!(tracker.paths(), Some(std::collections::HashSet::new()));
+        assert_eq!(tracker.state(), State::Clean);
+    }
+
+    #[test]
+    fn test_in_subpath_control_only() {
+        let td = tempfile::tempdir().unwrap();
+
+        let tree = create_standalone_workingtree(td.path(), &ControlDirFormat::default()).unwrap();
+        let subdir = td.path().join("subdir");
+        std::fs::create_dir(&subdir).unwrap();
+        let mut tracker =
+            DirtyTreeTracker::new_in_subpath(tree.clone(), std::path::Path::new("subdir"));
+        tree.commit(
+            "Dummy",
+            Some(true),
+            Some("Joe Example <joe@example.com>)"),
+            None,
+        )
+        .unwrap();
+        assert_eq!(tracker.relpaths(), Some(std::collections::HashSet::new()));
+        assert_eq!(tracker.state(), State::Clean);
         assert_eq!(tracker.paths(), Some(std::collections::HashSet::new()));
     }
 }
