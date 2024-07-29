f0094c38bd082e5b85ba942dc5357fb6cf4d83b0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-07-29 19:44:23 +0000

    add test infra

---
+++ src/tests.rs
@@ -0,0 +1,58 @@
+use std::fs;
+use std::path::PathBuf;
+use tempfile::TempDir;
+
+pub struct TestEnv {
+    pub temp_dir: TempDir,
+    pub working_dir: PathBuf,
+    pub home_dir: PathBuf,
+    pub old_cwd: PathBuf,
+    pub old_home: Option<String>,
+    pub old_email: Option<String>,
+}
+
+impl TestEnv {
+    pub fn new() -> Self {
+        let temp_dir = TempDir::new().unwrap();
+        let working_dir = temp_dir.path().join("test");
+        fs::create_dir(&working_dir).unwrap();
+        let home_dir = temp_dir.path().join("home");
+        fs::create_dir(&home_dir).unwrap();
+        let old_cwd = std::env::current_dir().unwrap();
+        let old_home = std::env::var("HOME").ok();
+        let old_email = std::env::var("EMAIL").ok();
+        std::env::set_current_dir(&working_dir).unwrap();
+        std::env::set_var("HOME", &home_dir);
+        std::env::set_var("EMAIL", "Joe Tester <joe@example.com>");
+        Self {
+            temp_dir,
+            home_dir,
+            working_dir,
+            old_cwd,
+            old_home,
+            old_email,
+        }
+    }
+}
+
+impl Drop for TestEnv {
+    fn drop(&mut self) {
+        if let Some(dir) = self.old_home.as_ref() {
+            std::env::set_var("HOME", dir);
+        } else {
+            std::env::remove_var("HOME");
+        }
+        if let Some(email) = self.old_email.as_ref() {
+            std::env::set_var("EMAIL", email);
+        } else {
+            std::env::remove_var("EMAIL");
+        }
+        std::env::set_current_dir(&self.old_cwd).unwrap();
+    }
+}
+
+impl Default for TestEnv {
+    fn default() -> Self {
+        Self::new()
+    }
+}
