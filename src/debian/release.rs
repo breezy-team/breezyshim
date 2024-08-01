34cf71024aa98e7508efca5da704455e67c6a60a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-08-01 20:55:18 +0000

    Split out ReleaseError

--- src/debian/release.rs
+++ src/debian/release.rs
@@ -2,11 +2,41 @@ use crate::error::Error;
 use crate::tree::MutableTree;
 use pyo3::prelude::*;
 
-pub fn release(local_tree: &dyn MutableTree, subpath: &std::path::Path) -> Result<String, Error> {
+#[derive(Debug)]
+pub enum ReleaseError {
+    GeneratedFile,
+    BrzError(Error),
+}
+
+impl From<Error> for ReleaseError {
+    fn from(err: Error) -> Self {
+        ReleaseError::BrzError(err)
+    }
+}
+
+impl std::fmt::Display for ReleaseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            ReleaseError::GeneratedFile => write!(f, "Generated file"),
+            ReleaseError::BrzError(err) => write!(f, "{}", err),
+        }
+    }
+}
+
+impl std::error::Error for ReleaseError {}
+
+pub fn release(
+    local_tree: &dyn MutableTree,
+    subpath: &std::path::Path,
+) -> Result<String, ReleaseError> {
+    pyo3::import_exception!(debmutate.reformatting, GeneratedFile);
     Python::with_gil(|py| {
         let m = py.import_bound("breezy.plugins.debian.release")?;
         let release = m.getattr("release")?;
-        let result = release.call1((local_tree.to_object(py), subpath))?;
-        Ok(result.extract()?)
+        match release.call1((local_tree.to_object(py), subpath)) {
+            Ok(result) => Ok(result.extract()?),
+            Err(err) if err.is_instance_of<GeneratedFile>() => Err(ReleaseError::GeneratedFile),
+            Err(err) => Err(ReleaseError::BrzError(err.into())),
+        }
     })
 }
