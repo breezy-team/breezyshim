f4792b91cf9d8e99b005d26f79dc9ac10464db82
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-08-03 12:37:03 +0000

    Allow errors in find_repository/open_repository

--- src/controldir.rs
+++ src/controldir.rs
@@ -54,20 +54,14 @@ impl ControlDir {
 
     pub fn open_repository(&self) -> Result<Repository, Error> {
         Python::with_gil(|py| {
-            let result = self
-                .to_object(py)
-                .call_method0(py, "open_repository")
-                .unwrap();
+            let result = self.to_object(py).call_method0(py, "open_repository")?;
             Ok(Repository::new(result))
         })
     }
 
     pub fn find_repository(&self) -> Result<Repository, Error> {
         Python::with_gil(|py| {
-            let result = self
-                .to_object(py)
-                .call_method0(py, "find_repository")
-                .unwrap();
+            let result = self.to_object(py).call_method0(py, "find_repository")?;
             Ok(Repository::new(result))
         })
     }
