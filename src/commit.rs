e5fe1c4408f8cb0f1cb43e1e70babff3007dd05f
Jelmer Vernooij <jelmer@jelmer.uk>
2024-09-20 00:03:01 +0000

    Implement ReportCommitToLog

--- src/commit.rs
+++ src/commit.rs
@@ -29,3 +29,45 @@ pub trait CommitReporter: ToPyObject {
 
 impl CommitReporter for NullCommitReporter {
 }
+
+pub struct ReportCommitToLog(PyObject);
+
+impl ReportCommitToLog {
+    pub fn new() -> Self {
+        Python::with_gil(|py| {
+            let m = py.import_bound("breezy.commit").unwrap();
+            let rctl = m.getattr("ReportCommitToLog").unwrap();
+            ReportCommitToLog(rctl.call0().unwrap().into())
+        })
+    }
+}
+
+impl From<PyObject> for ReportCommitToLog {
+    fn from(obj: PyObject) -> Self {
+        ReportCommitToLog(obj)
+    }
+}
+
+impl ToPyObject for ReportCommitToLog {
+    fn to_object(&self, py: Python) -> PyObject {
+        self.0.clone_ref(py)
+    }
+}
+
+impl CommitReporter for ReportCommitToLog {
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_null_commit_reporter() {
+        NullCommitReporter::new();
+    }
+
+    #[test]
+    fn test_report_commit_to_log() {
+        ReportCommitToLog::new();
+    }
+}
