37a1f92062ce8809655303433a063a0926febfa3
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-06-04 01:03:54 +0100

    Add prelude

---
+++ src/prelude.rs
@@ -0,0 +1,7 @@
+//! Prelude for the breezyshim crate
+//!
+//! This module re-exports commonly used items from the crate,
+pub use crate::branch::Branch;
+pub use crate::controldir::ControlDir;
+pub use crate::repository::Repository;
+pub use crate::tree::Tree;
