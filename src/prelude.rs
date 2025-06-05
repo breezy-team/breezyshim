aba4aff43c696c3e98bcff3fb5764dcb2616c80c
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-06-05 10:56:47 +0000

    Expose Error as BrzError in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,6 +3,7 @@
 //! This module re-exports commonly used items from the crate,
 pub use crate::branch::Branch;
 pub use crate::controldir::ControlDir;
+pub use crate::error::Error as BrzError;
 pub use crate::repository::Repository;
 pub use crate::tree::{MutableTree, Tree};
 pub use crate::workingtree::WorkingTree;
