d9ae832143b31c33b68becd693df672896fec9b0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-06-05 12:03:43 +0100

    Add RevisionId to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -5,5 +5,6 @@ pub use crate::branch::Branch;
 pub use crate::controldir::ControlDir;
 pub use crate::error::Error as BrzError;
 pub use crate::repository::Repository;
+pub use crate::revisionid::RevisionId;
 pub use crate::tree::{MutableTree, Tree};
 pub use crate::workingtree::WorkingTree;
